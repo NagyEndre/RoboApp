8f13d63c9e4f4f8ea84fe3aa3c291c919124aa4e
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-10 19:13:05 +0200

    FEAT: minimize JointDisplay height

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -18,6 +18,7 @@ export default class BaseCard extends Vue {
 section {
   box-shadow: 0 2px 4px grey;
   width: 45%;
+  height: min-content;
   padding: 1rem;
   border-radius: 0.5rem;
   display: inline-block;
@@ -27,7 +28,7 @@ h2 {
   border-bottom: 2px solid orange;
   margin-bottom: 1rem;
 }
-@media screen and (max-width: 640px) {
+@media screen and (max-width: 800px) {
   section {
     width: 80%;
   }
