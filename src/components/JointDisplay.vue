a49b18df7d2807361b54b0a6dd634641e4888d20
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-10 19:00:51 +0200

    FEAT: Create JointDisplay component

---
+++ src/components/JointDisplay.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="(joint, index) in joints" :key="index">
+      <span v-for="(value, key) in joint" :key="key">
+        {{ key }}: {{ value }}</span
+      >
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class JointDisplay extends Vue {
+  get joints() {
+    return this.$store.getters.joints
+  }
+}
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
