f9878c8744bf7c1d8c198960b300f2659e593c00
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-10 19:00:33 +0200

    FEAT: Add joints to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,19 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Joint from '@/logic/Joint'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    joints: [new Joint(1, 2, 3), new Joint(4, 5, 6), new Joint(7, 8, 9)],
+  },
+  getters: {
+    joints(state) {
+      return state.joints
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
-});
+})
