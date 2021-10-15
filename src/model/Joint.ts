dc72d9635b054dc29bf791d79b29707ed19650a0
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-15 14:45:41 +0200

    FEAT(Joint): add simultaneous setter for coordinates

--- src/model/Joint.ts
+++ src/model/Joint.ts
@@ -1,3 +1,9 @@
 export default class Joint {
   constructor(public x: number, public y: number, public z: number) {}
+
+  set(x: number, y: number, z: number) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
 }
