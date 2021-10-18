5de9e482325cb5c3905c5c5f322cccacb560115f
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-18 13:52:00 +0200

    FEAT: Add store constants

---
+++ src/store/store.const.ts
@@ -0,0 +1,11 @@
+export const GETTERS = {
+  JOINTS: 'joints',
+  ROBOT: 'robot',
+  FINGERS: 'fingers',
+}
+
+export const MUTATIONS = {
+  SET_JOINT_COORDINATES: 'setJointCoordinates',
+  OPEN_FINGER: 'openFinger',
+  CLOSE_FINGER: 'closeFinger',
+}
