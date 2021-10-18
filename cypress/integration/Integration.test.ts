d1f68fb0a62ee6a6eb7e0b8ef06b64784017c856
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-18 14:18:07 +0200

    TEST: add open all test case and multiple movements

--- cypress/integration/Integration.test.ts
+++ cypress/integration/Integration.test.ts
@@ -4,29 +4,59 @@ describe('RoboApp test suite', () => {
   beforeEach('Connect to page', () => {
     cy.visit('http://localhost:8080/')
   })
-  it('When single joint move command executed, Should set corresponding joint coordinates', () => {
-    // Act
+
+  it('When joint moved, should update corresponding coordinates', () => {
     cy.get('textarea').type('move 3 66 77 88')
     cy.get('button').click()
 
-    // Assert
     cy.get('#joint-display')
       .contains('Joint 3')
       .should('have.text', ' Joint 3  x: 66 y: 77 z: 88')
   })
-  it('When close single finger command executed, should close corresponding finger', () => {
-    // Act
+
+  it('When multiple joints moved, should update coordinates', () => {
+    cy.get('textarea').type('move 3 66 77 88\nmove 4 11 22 33')
+    cy.get('button').click()
+
+    cy.get('#joint-display').then((joints) => {
+      cy.wrap(joints)
+        .contains('Joint 3')
+        .should('have.text', ' Joint 3  x: 66 y: 77 z: 88')
+      cy.wrap(joints)
+        .contains('Joint 4')
+        .should('have.text', ' Joint 4  x: 11 y: 22 z: 33')
+    })
+  })
+
+  it('When finger closed, should display closed corresponding finger', () => {
     cy.get('textarea').type('close 4')
     cy.get('button').click()
 
-    // Assert
     cy.get('#tool-display')
       .find('li')
       .contains('Finger 4')
       .should('have.text', ' Finger 4  Closed ')
   })
-  it('When close all finger command executed, should close all fingers', () => {
+
+  it('When all fingers closed, should display closed fingers', () => {
     cy.get('textarea').type('close all')
     cy.get('button').click()
+
+    cy.get('#tool-display')
+      .find('li')
+      .each((item) => {
+        expect(item).to.contain('Closed')
+      })
+  })
+
+  it('When all fingers opened, should display opened fingers', () => {
+    cy.get('textarea').type('open all')
+    cy.get('button').click()
+
+    cy.get('#tool-display')
+      .find('li')
+      .each((item) => {
+        expect(item).to.contain('Open')
+      })
   })
 })
