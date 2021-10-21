ca9597328907689237b41e7c8a3074a52fa87548
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-21 10:10:25 +0200

    REFACTOR: switch Tool  to RobotHand

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    robot: buildRobot(createGripper()),
+    robot: buildRobot(createRobotHand()),
   },
   getters: {
     joints(state) {
