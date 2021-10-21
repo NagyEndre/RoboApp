2a827b2dbc13143e80c62efc2a1e9e4577c618f5
Endre Nagy <Endre.Nagy@kuka.com>
2021-10-21 11:06:10 +0200

    FEAT: Register BaseCard globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import BaseCard from '@/components/BaseCard.vue'
 
 Vue.config.productionTip = false
+Vue.component('base-card', BaseCard)
 
 new Vue({
   store,
